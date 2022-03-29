d8ca2635dc3dfd285018bf9eb6b7aee7192804de
yuto-osawa6 <pooh.atgmw628@gmail.com>
2022-03-29 18:13:27 +0900

    change

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,14 +1,17 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import { Provider } from "react-redux"
-import store from '../store'
+// import '../styles/globals.scss'
+import { AppProps } from 'next/app'
+import 'styles/globals.scss'
+// import type { AppProps } from 'next/app'
+// import { Provider } from "react-redux"
+// import store from '../store'
 // import store from "store/"
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <Provider store={store}>  
-      <Component {...pageProps} />
-    </Provider>
+    // <Provider store={store}>  
+    //   <Component {...pageProps} />
+    // </Provider>
+    <Component {...pageProps} />
   )
 }
 
