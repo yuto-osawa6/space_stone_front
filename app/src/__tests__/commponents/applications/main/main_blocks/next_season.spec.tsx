c0d2c29f7df62c09a5fea01ff6f754faf4676cb2
yuto-osawa6 <pooh.atgmw628@gmail.com>
2022-04-27 14:31:11 +0900

    calendar test season test

---
+++ app/src/__tests__/commponents/applications/main/main_blocks/next_season.spec.tsx
@@ -0,0 +1,67 @@
+import '@testing-library/jest-dom'
+import React from 'react'
+import {render, fireEvent, waitFor, screen, cleanup} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { server } from '@/mocks/server';
+import "whatwg-fetch"
+import { ThisSeasonAnimeInfomation } from '@/components/mains/main_block/ThisSeasonAnimeInfomation';
+import { product } from '@/interfaces/product';
+import { testProductData } from '@/mocks/data/d_product'
+import { Provider } from 'react-redux';
+import store from '@/store';
+import { NextSeasonAnimeInfomation } from '@/components/mains/main_block/NextSeasonAnimeInfomation';
+
+describe("this season list", () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  test('next season', async() => {
+    const d_products:product[] = [testProductData]
+    const d_products2:product[] = [testProductData]
+    const data:any = {
+        products:d_products,
+        products2:d_products2,
+        currentSeason:"test",
+        currentSeason2:"test2",
+        scores: {
+          avgScore: {1:"70"},
+          avgScore2: {1:"70"}
+      }
+    }
+      render(
+        <Provider store={store}>
+        <NextSeasonAnimeInfomation
+          data={data}
+        >
+        </NextSeasonAnimeInfomation>
+        </Provider>
+      );
+    // screen.debug();
+  });
+
+  test('next season no data', async() => {
+    const d_products:product[] = []
+    const d_products2:product[] = []
+
+    const data:any = {
+      products:d_products,
+      products2:d_products2,
+      currentSeason:"",
+      currentSeason2:"",
+      scores: {
+        avgScore: {},
+        avgScore2: {}
+    }
+  }
+      render(
+        <Provider store={store}>
+        <NextSeasonAnimeInfomation
+          data={data}
+        >
+        </NextSeasonAnimeInfomation>
+        </Provider>
+      );
+    // screen.debug();
+  });
+});
