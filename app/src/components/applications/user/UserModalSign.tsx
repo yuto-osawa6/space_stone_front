2fdc6a4d39e009ed408031d13df7252d76eb44ef
yuto-osawa6 <pooh.atgmw628@gmail.com>
2022-05-27 14:23:51 +0900

    k

--- app/src/components/applications/user/UserModalSign.tsx
+++ app/src/components/applications/user/UserModalSign.tsx
@@ -31,7 +31,12 @@ export const UserModalSign:React.FC = function UserModalSign(){
   console.log(process.env.NEXT_PUBLIC_P_GOOGLE_KEY)
   console.log(process.env)
   console.log("aaaaa")
-  console.log(ENV["NEXT_PUBLIC_A1"])
+  console.log(process.env.MY_ENV_VAR)
+  console.log(process.env.MY_ENV_VAR2)
+  console.log(process.env.API_ORIGIN4)
+  // console.log(MY_ENV_VAR2)
+
+  // console.log(ENV["NEXT_PUBLIC_A1"])
   return (
     <>
       <Modal
