c96c0698b8d6ad33ae335154047d10307a1cf3f4
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2022-06-05 03:10:03 +0900

    Update GoogleButton.tsx

--- app/src/components/applications/user/GoogleButton.tsx
+++ app/src/components/applications/user/GoogleButton.tsx
@@ -4,8 +4,28 @@ export const GoogleButton:React.FC = function Btn(){
   // pathname
   const router = useRouter()
   // console.log(router)
-  
-  return <a href={`${process.env.NEXT_PUBLIC_AUTH_URL}${router.pathname}`}>Sign in with Google</a>
+  const handleClickGoogle = () => {
+    let blankForm = document.createElement('form')
+    blankForm.method = 'post'
+    blankForm.action = `${process.env.NEXT_PUBLIC_AUTH_URL}${router.pathname}`
+    // blankForm.action = `http://localhost:3001/auth/google_oauth2${router.pathname}`
 
+    blankForm.style.display = 'none'
+    document.body.appendChild(blankForm)
+    blankForm.submit()
+    console.log(blankForm)
+    document.body.removeChild(blankForm)
+  }
+  
+  return (
+    <>
+      <a href={`${process.env.NEXT_PUBLIC_AUTH_URL}${router.pathname}`}>Sign in with Google</a>
+
+      <p><button onClick={handleClickGoogle}>clickButton</button></p>
+
+      {/* <form action={`${process.env.NEXT_PUBLIC_AUTH_URL}${router.pathname}`} method="post">
+        <p><input type="submit" name="submit" value="clickaggghh" /></p>
+      </form> */}
+    </>
+  )
 }
-// ${process.env.NEXT_PUBLIC_AUTH_URL}
