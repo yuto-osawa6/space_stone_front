6e77fdd63dbcba088f942a98cbbf5c3d8454e912
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2022-06-06 08:14:43 +0900

    Update GoogleButton.tsx

--- app/src/components/applications/user/GoogleButton.tsx
+++ app/src/components/applications/user/GoogleButton.tsx
@@ -7,8 +7,8 @@ export const GoogleButton:React.FC = function Btn(){
   const handleClickGoogle = () => {
     let blankForm = document.createElement('form')
     blankForm.method = 'post'
-    blankForm.action = `${process.env.NEXT_PUBLIC_AUTH_URL}${router.pathname}`
-    // blankForm.action = `http://localhost:3001/auth/google_oauth2${router.pathname}`
+    // blankForm.action = `${process.env.NEXT_PUBLIC_AUTH_URL}${router.pathname}`
+    // blankForm.action = `http://localhost:3001/omniauth/google_oauth2${router.pathname}`
 
     blankForm.style.display = 'none'
     document.body.appendChild(blankForm)
@@ -21,7 +21,7 @@ export const GoogleButton:React.FC = function Btn(){
     <>
       <a href={`${process.env.NEXT_PUBLIC_AUTH_URL}${router.pathname}`}>Sign in with Google</a>
 
-      <p><button onClick={handleClickGoogle}>clickButton</button></p>
+      {/* <p><button onClick={handleClickGoogle}>clickButton</button></p> */}
 
       {/* <form action={`${process.env.NEXT_PUBLIC_AUTH_URL}${router.pathname}`} method="post">
         <p><input type="submit" name="submit" value="clickaggghh" /></p>
