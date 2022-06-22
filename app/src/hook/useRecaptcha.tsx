acd8bf4bbfd86f95c6b1b520651442e83328dd9d
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2022-06-22 21:57:51 +0900

    Update useRecaptcha.tsx

--- app/src/hook/useRecaptcha.tsx
+++ app/src/hook/useRecaptcha.tsx
@@ -3,16 +3,20 @@ import { useGoogleReCaptcha } from 'react-google-recaptcha-v3'
 
 
 export const execSetreCaptchaToken = async() => {
-  if (process.env.NODE_ENV == "production"){
+  // if (process.env.NODE_ENV == "production"){
   const { executeRecaptcha } = useGoogleReCaptcha()
     if (!executeRecaptcha) {
+
+      console.log("rev")
       return
     }
+    // console.log("rev")
+    console.log("n")
     const reCaptchaToken = await executeRecaptcha('ReviewModal');
     console.log(reCaptchaToken)
   return reCaptchaToken
-  }else{
-    return "local"
-  }
+  // }else{
+  //   return "local"
+  // }
 }
 
